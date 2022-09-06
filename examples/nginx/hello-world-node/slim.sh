bd3201475b01cf26515ee1a58314219a409ff3f5
Redocnib <kiran.txt@gmail.com>
2022-09-06 21:17:55 +0530

    Update

--- examples/nginx/hello-world-node/slim.sh
+++ examples/nginx/hello-world-node/slim.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-image_name1=nginx-node-hello-nginx-server
-image_name2=nginx-node-hello-node-server
+image_name1=johnnyslim/dcexample:nginx
+image_name2=johnnyslim/dcexample:node
 docker build -t $image_name1 .
-docker build -t $image_name2 .
+docker build -t $image_name2 ./node-app/.
 
 #Resuires docker-slim: https://github.com/docker-slim/docker-slim
-docker-slim build \
---compose-file=docker-compose.yml \
---target=$image_name1 \
---http-probe=true \
---expose 80
+# docker-slim build \
+# --compose-file=docker-compose.yml \
+# --target=$image_name1 \
+# --http-probe=true \
+# --expose 80
 
 #Requires grype: https://github.com/anchore/grype
 # grype $image_name.slim > $image_name.slim.vul.txt
